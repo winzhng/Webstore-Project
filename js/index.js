758973993522f3596a03e18bc516977a918cb546
winzhng <winniee.zhangg@gmail.com>
2019-12-08 21:39:41 -0500

    commit3

--- js/index.js
+++ js/index.js
@@ -3,41 +3,265 @@ let products = [{
     name:`Cotton Romper with Waist Tie`,
     image: `img/romper.jpg`,
     description: `Super chic romper for those casual days. Breezy and light for the summer, pair with a white sneaker or wedges for an elevated look!`,
+    color1: `White`,
+    color2: `Pink`,
+    color3: `Light Blue`,
+    color4: null,
     price: `$39.00`,
-    quantity: 250,
-    category: `jumpsuits`
+    quantity: 35,
+    category: `Jumpsuits`
 },{
     id: 001,
     name:`Embroidered Mesh Dress`,
     image: `img/embrod_front.jpg`,
     description: `Simply perfect embroidered mesh midi dress. Made from a lightweight cotton blend and mesh lining with zipper closure at back and topped with a v-neckline. Could be dressed up or down for various occassions.`,
+    color1: `White`,
+    color2: null,
+    color3: null,
+    color4: null,
     price: `$50.00`,
-    quantity: 200,
-    category: `dresses`
+    quantity: 40,
+    category: `Dresses`
 },{
     id: 002,
     name:`Striped Maxi Dress with Slit`,
     image: `img/stripes.jpg`,
     description: `Soft, comfortable cotton for a relaxing fit. Front-tie waist and leg slit for a chic, feminine look.`,
+    color1: `White`,
+    color2: null,
+    color3: null,
+    color4: null,
     price: `$55.00`,
-    quantity: 300,
-    category: `dresses`
+    quantity: 15,
+    category: `Dresses`
 },{
     id: 003,
-    name:`Black Floral Maxi Dress`,
-    image: `img/floraldress.jpg`,
-    description: `Flowy every-day maxi dress made with medium-weight fabric. Wrapping, maxi skirt secured with internal ties and waist sash.`,
-    price: `$30.00`,
-    quantity: 200,
-    category: `dresses`
+    name:`Moto Jacket`,
+    image: `img/motojacket.jpeg`,
+    description: `Sturdy faux leather jacket. This must-have statement piece is perfect for any closet`,
+    color1: `Black`,
+    color2: null,
+    color3: null,
+    color4: null,
+    price: `$100.00`,
+    quantity: 60,
+    category: `Coats & Jackets`
 },{
     id: 004,
     name:`Two Piece Off-White Set`,
     image: `img/coord.jpg`,
     description: `Style this together or separate! Vintage style halter top and front slit a-line skirt!`,
+    color1: `Ivory`,
+    color2: null,
+    color3: null,
+    color4: null,
     price: `$45.00`,
-    quantity: 400,
-    category: 'jumpsuits'
+    quantity: 10,
+    category: 'Jumpsuits'
+},{
+    id: 005,
+    name:`Sequin Longsleeve Bridesmaid Dress`,
+    image: `img/bridesmaid.jpg`,
+    description: `Bridesmaid dress with champagne coloured sequin long-sleeve and pleated chiffon.`,
+    color1: `Pink`,
+    color2: null,
+    color3: null,
+    color4: null,
+    price: `$250.00`,
+    quantity: 80,
+    category: `Dresses`
+},{
+    id: 006,
+    name:`Denim Sherpa Lining Jacket`,
+    image: `img/denimsherpa.png`,
+    description: `Medium light wash denim jacket with a cozy sherpa inner lining for those colder days!`,
+    color1: `Medium blue`,
+    color2: null,
+    color3: null,
+    color4: null,
+    price: `$65.00`,
+    quantity: 30,
+    category: `Coats & Jackets`
+},{
+    id: 007,
+    name:`Floral Embroidered Boho Jumpsuit`,
+    image: `img/floralboho.jpeg`,
+    description: `Beautiful floral boho style jumpsuit`,
+    color1: `White`,
+    color2: null,
+    color3: null,
+    color4: null,
+    price: `$45.00`,
+    quantity: 30,
+    category: `Jumpsuits`
+},{
+    id: 008,
+    name:`Long Puffer Jacket`,
+    image: `img/longpuffer.jpg`,
+    description: `Made with goose filled down, this long puffer jacket has a fur lined hood and this timeless piece will keep you warm on those brisk winter days`,
+    color1: `Black`,
+    color2: `White`,
+    color3: `Taupe`,
+    color4: `Forest green`,
+    price: `$175.00`,
+    quantity: 12,
+    category: `Coats & Jackets`
+},{
+    id: 009,
+    name:`Metallic Jumpsuit`,
+    image: `img/metallichalter.jpg`,
+    description: `Great semi-formal and formal party wear. Halter  neck tie and wide leg pleats.`,
+    color1: `Silver`,
+    color2: `Rose gold`,
+    color3: null,
+    color4: null,
+    price: `$65.00`,
+    quantity: 25,
+    category: `Jumpsuits`
+},{
+    id: 0010,
+    name:`Black Floral Maxi Dress`,
+    image: `img/floraldress.jpg`,
+    description: `Flowy every-day maxi dress made with medium-weight fabric. Wrapping, maxi skirt secured with internal ties and waist sash.`,
+    color1: `Black`,
+    color2: null,
+    color3: null,
+    color4: null,
+    price: `$30.00`,
+    quantity: 56,
+    category: `Dresses`
+},{
+    id: 0011,
+    name:`Oversized Brushed Wool Coat`,
+    image: `img/oversizedwool.jpeg`,
+    description: `Brushed llama wool trench coat, oversized fit with large lapels. Equipped with large pockets to store your must-haves when you're on the go.`,
+    color1: `Camel`,
+    color2: `Ivory`,
+    color3: `Black`,
+    color4: null,
+    price: `$130.00`,
+    quantity: 21,
+    category: `Coats & Jackets`
+},{
+    id: 0012,
+    name:`Structured Pearl Blazer`,
+    image: `img/pearlblazer.jpg`,
+    description: `Flowy every-day maxi dress made with medium-weight fabric. Wrapping, maxi skirt secured with internal ties and waist sash.`,
+    color1: `Navy`,
+    color2: `Black`,
+    color3: `Grey`,
+    color4: null,
+    price: `$40.00`,
+    quantity: 30,
+    category: `Coats & Jackets`
+},{
+    id: 0013,
+    name:`Pink Pearl Brushed Trench`,
+    image: `img/pinkpearl.jpg`,
+    description: `Brushed llama wool trench coat, oversized fit with zipper closure. Stitched pearls details on the waist.`,
+    color1: `Pink`,
+    color2: null,
+    color3: null,
+    color4: null,
+    price: `$120.00`,
+    quantity: 33,
+    category: `Coats & Jackets`
+},{
+    id: 0014,
+    name:`White Floral Ruffled Dress`,
+    image: `img/rufflefloral.jpg`,
+    description: `Short sleeve ruffled chiffon dress; easy and breezy.`,
+    color1: `White`,
+    color2: null,
+    color3: null,
+    color4: null,
+    price: `$75.00`,
+    quantity: 38,
+    category: `Dresses`
+},{
+    id: 0015,
+    name:`Wrapped Waist-Tie Green Jumpsuit`,
+    image: `img/wrapgreen.jpg`,
+    description: `Jersey cotton jumpsuit with short lapels, button-up and waist tie closure.`,
+    color1: `Forest green`,
+    color2: null,
+    color3: null,
+    color4: null,
+    price: `$65.00`,
+    quantity: 32,
+    category: `Jumpsuits`
+},{
+    id: 0016,
+    name:`Leopard Print Midi Dress`,
+    image: `img/leopardprint.jpeg`,
+    description: `Cotton leopard print midi dress.`,
+    color1: `Black`,
+    color2: null,
+    color3: null,
+    color4: null,
+    price: `$45.00`,
+    quantity: 22,
+    category: `Dresses`
+},{
+    id: 0017,
+    name:`Teddy Coat Trench`,
+    image: `img/teddycoat.jpg`,
+    description: `Plush teddy trench coat with large lapels.`,
+    color1: `Camel`,
+    color2: `Black`,
+    color3: null,
+    color4: null,
+    price: `$115.00`,
+    quantity: 40,
+    category: `Coats & Jackets`
+},{
+    id: 0018,
+    name:`Denim Utility Jumpsuit`,
+    image: `img/whitedenim.jpeg`,
+    description: `White denim utility jumpsuit. Made with thick, tough denim durable for workwear.`,
+    color1: `Ivory`,
+    color2: null,
+    color3: null,
+    color4: null,
+    price: `$75.00`,
+    quantity: 30,
+    category: `Jumpsuits`
+},{
+    id: 0019,
+    name:`Sleek Green Satin Midi Dress`,
+    image: `img/sleekgreen.png`,
+    description: `Sleek satin midi dress with crowl neck line and silky satin fabric.`,
+    color1: `Forest green`,
+    color2: null,
+    color3: null,
+    color4: null,
+    price: `$85.00`,
+    quantity: 32,
+    category: `Dresses`
+},{
+    id: 0020,
+    name:`Checkered Trench Coat`,
+    image: `img/checkeredcoat.jpeg`,
+    description: `Checkered trench coat with large lapels and button closures.`,
+    color1: `Forest green`,
+    color2: null,
+    color3: null,
+    color4: null,
+    price: `$125.00`,
+    quantity: 63,
+    category: `Coats & Jackets`
+},{
+    id: 0021,
+    name:`Pleated Crop Dusty Pink Jumpsuit`,
+    image: `img/pleatedcrop.jpeg`,
+    description: `Pleated crop top and wide leg bottom jumpsuit in a beautiful dusty pink.`,
+    color1: `Dusty Pink`,
+    color2: null,
+    color3: null,
+    color4: null,
+    price: `$75.00`,
+    quantity: 76,
+    category: `Jumpsuits`
 }]
 
 function renderProducts(arr) {
@@ -50,7 +274,7 @@ function getProductsAsHtmlString(product){
     return `
     <article class="product">
             <div class="product_image">
-              <img src=${product.image} alt="Romper">
+              <img src=${product.image} alt="romper">
             </div>
             <section class="product2">
               <h3>${product.name}</h3>
@@ -59,9 +283,10 @@ function getProductsAsHtmlString(product){
                 <fieldset>
                   <legend>Colours</legend>
                   <ul>
-                    <li><label><input type="radio" name="colour" value="p"> <span>Pink</span></label></li>
-                    <li><label><input type="radio" name="colour" value="w"> <span>White</span></label></li>
-                    <li><label><input type="radio" name="colour" value="lb"> <span>Light Blue</span></label></li>
+                    <li><label><input type="radio" name="color" value="p"> <span>${product.color1}</span></label></li>
+                    <li><label><input type="radio" name="color" value="w"> <span>${product.color2}</span></label></li>
+                    <li><label><input type="radio" name="color" value="w"> <span>${product.color3}</span></label></li>
+                    <li><label><input type="radio" name="color" value="w"> <span>${product.color4}</span></label></li>
                   </ul>
                 </fieldset>
 
@@ -83,13 +308,13 @@ function getProductsAsHtmlString(product){
                     <dd>4.0 <span class="material-icons">star</span><span class="material-icons">star</span><span class="material-icons">star</span><span class="material-icons">star</span></dd>
                   </dl>
                   <div>
-                      Quantity in stock: ${product.quantity}
+                      Quantity left in stock: ${product.quantity}
                   </div>
                   <div>
                     Category: ${product.category}
                   </div>
                   <data value="39"> <ins id="price">${product.price}</ins></data>
-                  <button type="button"><span class="material-icons">add_shopping_cart</span> Add to Cart</button>
+                  <button id="addCart" type="button"><span class="material-icons">add_shopping_cart</span> Add to Cart</button>
                   <button type="button"><span class="material-icons">favorite</span></button>
                   <a href="#">see more</a>
                 </footer>
@@ -100,3 +325,7 @@ function getProductsAsHtmlString(product){
 }
 
 renderProducts(products)
+
+document.getElementById("addCart").addEventListener("click", function() {
+    alert("Added to Cart!");
+  });
